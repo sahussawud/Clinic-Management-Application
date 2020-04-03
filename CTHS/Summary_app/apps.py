143615c9bbd6e95adea981c0f76f4dfea7f03866
bbb <borrabeer@outlook.com>
2020-04-03 22:23:02 +0700

    Create django project and apps
    Create django projects and apps.
    - User_app
    - Treatment
    - Medicine
    - Summary_app

---
+++ CTHS/Summary_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SummaryAppConfig(AppConfig):
+    name = 'Summary_app'
---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Administrator\Desktop\Clinic-Treatment-History-System\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
