d7751a532d78199f6e518afe84a131f983297469
sahussawud <sahussawud@hotmail.com>
2020-04-19 21:06:44 +0700

    create create patient page

--- CTHS/Treatment/urls.py
+++ CTHS/Treatment/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from Treatment import views
   
 urlpatterns = [ 
-    path('/patient', views.home_patient, name = 'home_patient'),
+    path('patient/', views.home_patient, name = 'home_patient'),
+    path('patient/create/', views.create_patient, name = 'create_patient'),
 
 ] 
--- CTHS/Treatment/views.py
+++ CTHS/Treatment/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home_patient(request):
     return render(request, 'Treatment/home_patient.html')
+
+def create_patient(request):
+    return render(request, 'Treatment/create_patient.html')
