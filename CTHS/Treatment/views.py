985bdbb5f086d07bdbc1e8fb1140ef91bc36f5a0
Denpoom Srimuang <forizzle28@hotmail.com>
2020-04-22 18:16:57 +0700

    Revert "Merge branch 'master' of https://github.com/sahussawud/Clinic-Treatment-History-System"
    This reverts commit 09329e27d27b7914e5122ab8498da819b8f4e314, reversing
    changes made to e9ed532ff83ee7b454324d5a9ef0acf0baa45398.

--- CTHS/Treatment/views.py
+++ CTHS/Treatment/views.py
@@ -5,20 +5,6 @@ def home_patient(request):
     return render(request, 'Treatment/home_patient.html')
 
 def create_patient(request):
-    if request.method == 'POST':
-        firstName = request.POST.get('firstName')
-        lastName = request.POST.get('lastName')
-        ssid = request.POST.get('ssid')
-        dateofbirth = request.POST.get('dateofbirth')
-        nation = request.POST.get('nation')
-        mature_land = request.POST.get('mature_land')
-        patientRole = request.POST.get('patientRole')
-        patientStatus = request.POST.get('patientStatus')
-        ref_code = request.POST.get('ref_code')
-        bloodType = request.POST.get('bloodType')
-        address = request.POST.get('address')
-        province = request.POST.get('province')
-        zip = request.POST.get('zip')
     return render(request, 'Treatment/create_patient.html')
 
 def create_treatment(request, patient_id):
