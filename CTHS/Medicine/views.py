af603e882fb30a8f718f30a9d81fbcf02475fddf
Denpoom Srimuang <forizzle28@hotmail.com>
2020-04-23 00:07:32 +0700

    Create templates update medicine
    Create templates update medicine

--- CTHS/Medicine/views.py
+++ CTHS/Medicine/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def add_medicine(request):
     return render(request, 'Medicine/add_medicine.html')
+def update_medicine(request):
+    return render(request, 'Medicine/update_medicine.html')
