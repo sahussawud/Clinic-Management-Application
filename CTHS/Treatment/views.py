f196133863285508440bb0eadcb30cec5a7e125c
bbb <borrabeer@outlook.com>
2020-04-24 02:30:14 +0700

    update create patient method and make rest api

--- CTHS/Treatment/views.py
+++ CTHS/Treatment/views.py
@@ -1,7 +1,13 @@
+from datetime import datetime
+
 from django.shortcuts import redirect, render
 
-from User_app.models import PatientForm, Public_Health
+from User_app.models import Patient, PatientForm, Public_Health, Congenital_disease
 
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import Congenital_diseaseSerializer, Congenital_diseaseSerializerWithoutPatient
 
 # Create your views here.
 def home_patient(request):
@@ -17,7 +23,6 @@ def create_patient(request):
     if request.method == 'POST':
         form = PatientForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data)
             if request.user.id:
                 print(request.user.id)
                 new_patient = form.save(commit=False)
@@ -29,6 +34,75 @@ def create_patient(request):
     else:
         form = PatientForm()
     return render(request, 'Treatment/create_patient.html', {'form': form})
+
+def update_patient(request, patient_id):
+    context = {}
+    patient_data = Patient.objects.get(p_id=patient_id)
+    if request.method == 'POST':
+        form = PatientForm(request.POST, instance=patient_data)
+        context['form'] = form
+        if form.is_valid():
+            update_data = form.save(commit=False)
+            creator = patient_data.public_health_id
+            update_data.public_health_id = creator
+            update_data.save()
+            form.save_m2m()
+            context['form'] = PatientForm(instance=update_data)
+            context['update'] = True
+            context['p_id'] = patient_id
+            context['status'] = "อัพเดทข้อมูลเรียบร้อย !"
+    else:
+        if patient_data:
+            context['form'] = PatientForm(instance=patient_data)
+            context['update'] = True
+            context['p_id'] = patient_id
+            return render(request, 'Treatment/create_patient.html', context=context)
+        else:
+            context['form'] = PatientForm()
+            context['update'] = False
+    return render(request, 'Treatment/create_patient.html', context=context) 
+
+class Conginetal_diseaseAPIView(APIView):
+    def get(self, request, patient_id):
+        patient = Patient.objects.get(p_id=patient_id)
+        items = Congenital_disease.objects.filter(patient_id=patient)
+        serializer = Congenital_diseaseSerializer(items, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, patient_id):
+        print(request.data)
+        serializer = Congenital_diseaseSerializer(data=request.data)
+        if 'id' in request.data:
+            cd_data = Congenital_disease.objects.get(id=request.data['id'])
+            cd_data.patient_id.add(request.data['patient_id'])
+        if serializer.is_valid():
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def patch(self, request, patient_id):
+        print(request.data)
+        serializer = Congenital_diseaseSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class Conginetal_diseaseWithoutPatientAPIView(APIView):
+    def get(self, request):
+        items = Congenital_disease.objects.all()
+        serializer = Congenital_diseaseSerializerWithoutPatient(items, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        serializer = Congenital_diseaseSerializerWithoutPatient(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 def create_treatment(request, patient_id):
     return render(request, 'Treatment/create_treatment.html')
