dbd9f326b242b2a84beb3c2590610d16f52a4850
sahussawud <sahussawud@hotmail.com>
2020-04-23 18:06:08 +0700

    make home_medicine
    -link medicine related
    -mute sidebar

--- CTHS/Medicine/views.py
+++ CTHS/Medicine/views.py
@@ -3,5 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def add_medicine(request):
     return render(request, 'Medicine/add_medicine.html')
+
 def update_medicine(request):
     return render(request, 'Medicine/update_medicine.html')
+
+def home_medicine(request):
+    return render(request, 'Medicine/home_medicine.html')
