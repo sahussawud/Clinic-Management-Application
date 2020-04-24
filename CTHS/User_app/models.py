4ee846b5a3a63822b570d7e82c97ed616b3d1035
sahussawud <sahussawud@hotmail.com>
2020-04-24 22:25:58 +0700

    progress bar on patient profile

--- CTHS/User_app/models.py
+++ CTHS/User_app/models.py
@@ -140,49 +140,6 @@ class Patient(models.Model):
         return int((datetime.date.today() - self.birth_day).days / 365.25 )
     age = property(age)
 
-class PatientForm(ModelForm):
-    class Meta:
-        model = Patient
-        exclude = ['p_id', 'age', 'public_health_id', 'date']
-        labels = {
-            'fname': _('ชื่อ'),
-            'lname': _('นามสกุล'),
-            'idcard_number': _('รหัสบัตรประชาชน'),
-            'birth_day': _('วัน/เดือน/ปี เกิด'),
-            'race': _('เชื้อชาติ'),
-            'nationality': _('สัญชาติ'),
-            'status': _('สถานะ'),
-            'blood_type': _('กลุ่มเลือด'),
-            'phone': _('เบอร์โทรศัพท์'),
-            'address': _('ที่อยู่'),
-            'patient_role': _('สิทธิการรักษา'),
-            'id_code': _('รหัสประจำตัว'),
-            'hospital_refer': _('โรงพยาบาลที่ส่งต่อ'),
-            'gold_card_no': _('เลขที่บัตรทอง'),
-            'gold_card_expire': _('วันหมดอายุบัตรทอง'),
-        }
-        widgets = {
-            'fname': TextInput(attrs={'class': 'form-control'}),
-            'lname': TextInput(attrs={'class': 'form-control'}),
-            'idcard_number': TextInput(attrs={'class': 'form-control'}),
-            'birth_day': DateInput(attrs={'class': 'form-control', 'type': 'date'}),
-            'nationality': Select(attrs={'class': 'custom-select d-block w-100'}),
-            'race': Select(attrs={'class': 'custom-select d-block w-100'}),
-            'status': RadioSelect(attrs={'class': ''}),
-            'blood_type': Select(attrs={'class': 'custom-select d-block w-100'}),
-            'phone': TextInput(attrs={'class': 'form-control'}),
-            'address': Textarea(attrs={'class': 'form-control','rows':'4'}),
-            'patient_role': RadioSelect(attrs={'class': ''}),
-            'id_code': TextInput(attrs={'class': 'form-control'}),
-            'hospital_refer': TextInput(attrs={'class': 'form-control'}),
-            'gold_card_no': TextInput(attrs={'class': 'form-control'}),
-            'gold_card_expire': DateInput(attrs={'class': 'form-control', 'type': 'date' }),
-        }
-    def clean_birth_day(self):
-        current_date = datetime.now().date()
-        if self.cleaned_data['birth_day'] >= current_date:
-            raise forms.ValidationError("วัน/เดือน/ปี เกิดไม่ถูกต้อง !")
-        return self.cleaned_data['birth_day']
 
 class Congenital_disease(models.Model):
     name = models.CharField(_("Congenital disease name"), max_length=255)
