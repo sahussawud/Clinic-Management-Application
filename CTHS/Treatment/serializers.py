9c6c6993537273904e8d5082372b77f99a1af16d
bbb <borrabeer@outlook.com>
2020-04-25 16:33:35 +0700

    update api

--- CTHS/Treatment/serializers.py
+++ CTHS/Treatment/serializers.py
@@ -19,16 +19,16 @@ class Congenital_diseaseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Congenital_disease
         fields = ['id', 'name', 'patient_id']
-        read_only_fields = ['id']
+        read_only_fields = ['id', 'name']
 
 class DrugSerializer(serializers.ModelSerializer):
     class Meta:
         model = Drug
-        fields = ['med_sup_id', 'name', 'patient']
-        read_only_fields = ['med_sup_id', 'name', 'patient']
+        fields = ['med_sup_id', 'name']
+        read_only_fields = ['med_sup_id', 'name']
 
 class PatientSerializer(serializers.ModelSerializer):
     class Meta:
         model = Patient
-        fields = ['__all__']
+        fields = '__all__'
         read_only_fields = ['p_id']
--- CTHS/Treatment/views.py
+++ CTHS/Treatment/views.py
@@ -152,16 +152,18 @@ class DrugAPIView(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
     """
     ต้องการเพิ่มแพ้ยาจากรายชื่อยาที่มีอยู่
+    DATA REQUIRED : med_sup_id : <int: ID ของยาจาก model Drug>
+    *** สร้างประวัติการแพ้ยาจาก ID Patient ที่ส่งมาพร้อม Path เท่านั้น ***
     """
     def post(self, request, patient_id):
-        print(patient_id)
-        print(request.data)
         serializer = DrugSerializer(data=request.data)
+        patient_data = Patient.objects.get(p_id=patient_id)
         if 'med_sup_id' in request.data:
             drug_data = Drug.objects.get(med_sup_id=request.data['med_sup_id'])
-            patient_data = Patient.objects.get(p_id=patient_id)
             drug_data.patient.add(patient_data)
         if serializer.is_valid():
+            items = Drug.objects.filter(patient=patient_data).order_by('name')
+            serializer = DrugSerializer(items, many=True)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -170,19 +172,22 @@ class DrugAPIView(APIView):
 class DrugWithoutPatientAPIView(APIView):
     """ API ยาทั้งหมด """
     def get(self, request):
-        if request.GET.get('keywords'):
-            items = Drug.objects.filter(name__icontains=request.GET['keywords']).order_by('name')
+        if 'keywords' in request.data:
+            items = Drug.objects.filter(name__icontains=request.data['keywords']).order_by('name')
         else:
             items = Drug.objects.all().order_by('name')
         serializer = DrugSerializer(items, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 class PatientSearchAPIView(APIView):
-    """ API ค้นหาคนไข้ """
+    """ 
+    API ค้นหาคนไข้ 
+    DATA REQUIRED : search : <string: keyword ที่ต้องการค้นหาจาก ชื่อ - นามสกุล, รหัสบัตรประชาชน, เบอร์โทรศัพท์, หรือ รหัสประจำตัว>
+    """
     def get(self, request):
         print(request.data)
         items = Patient.objects.filter(Q(fname__icontains=request.data['search']) | Q(lname__icontains=request.data['search']) | Q(idcard_number__icontains=request.data['search'])
-        | Q(phone__icontains=request.data['search']) | Q(id_code__icontains=request.data['search']))
+        | Q(phone__icontains=request.data['search']) | Q(id_code__icontains=request.data['search'])).order_by('fname')
         serializer = PatientSerializer(items, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
