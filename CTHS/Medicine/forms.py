6c7d2a574da5303da5c75c1bbce804b9f52533a9
Denpoom Srimuang <forizzle28@hotmail.com>
2020-04-25 22:58:42 +0700

    model form add med
    เปลี่ยน add ยา ไปใช้ model form

---
+++ CTHS/Medicine/forms.py
@@ -0,0 +1,49 @@
+
+from django import forms
+from django.forms import ModelForm
+from django.forms.widgets import (Select, Textarea,TextInput)
+from Medicine.models import Drug,Med_supply
+
+
+
+
+
+
+
+
+class  MedicineSupplyForm(forms.ModelForm):
+    
+    class Meta: 
+        model =  Med_supply
+        fields = ('sup_id','name','amount','description')
+        widgets = {
+            'sup_id' : TextInput(attrs={'class': 'form-control form-control-sm'}),
+            'name' : TextInput(attrs={'class': 'form-control form-control-sm'}),
+            'amount': TextInput(attrs={'class': 'form-control form-control-sm'}),
+            'description': Textarea(attrs={'class': 'form-control form-control-sm','rows':'5',}),
+        }
+        lebels={
+            'sup_id': 'รหัสยา',
+            'name': 'ชื่อยา',
+            'amount': 'จำนวน',
+            'description': 'รายระเอียด',
+        }
+
+class  MedicineDrugForm(forms.ModelForm):
+    
+    class Meta: 
+        model =  Drug
+        fields = ('drug_id','name','amount','description')
+        widgets = {
+            'drug_id' : TextInput(attrs={'class': 'form-control form-control-sm'}),
+            'name' : TextInput(attrs={'class': 'form-control form-control-sm'}),
+            'amount': TextInput(attrs={'class': 'form-control form-control-sm'}),
+            'description': Textarea(attrs={'class': 'form-control form-control-sm','rows':'5',}),
+        }
+        lebels={
+            'drug_id': 'รหัสยา',
+            'name': 'ชื่อยา',
+            'amount': 'จำนวน',
+            'description': 'รายระเอียด',
+        }
+        
--- CTHS/Medicine/views.py
+++ CTHS/Medicine/views.py
@@ -1,38 +1,69 @@
 from http.client import HTTPResponse
 from urllib import request
 
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.decorators.csrf import csrf_exempt
-from Medicine.models import Drug,Med_supply
 
+from Medicine.models import Drug, Med_supply
+from Medicine.forms import MedicineSupplyForm
+from Medicine.forms import MedicineDrugForm
 
 # Create your views here.
 def add_medicine(request):
+    
     if request.method == 'POST':
+        form =  MedicineDrugForm(request.POST)
         
-        med_sup_id = request.POST.get('med_sup_id')
-        name = request.POST.get('name')
-        amount = request.POST.get('amount')
-        types = request.POST.get('types')
-        if types == 'Drug':
-            add_med = Drug(
-                med_sup_id=med_sup_id,
+        if form.is_valid():
+            drug_id = form.cleaned_data['drug_id']
+            name = form.cleaned_data['name']
+            amount = form.cleaned_data['amount']
+            description = form.cleaned_data['description']
+            post = Drug(
+                drug_id=drug_id,
                 name=name,
                 amount=amount,
-            )
-            add_med.save()
-        else:
-            add_med = Med_supply(
-                med_sup_id=med_sup_id,
-                name=name,
-                amount=amount,
-                
-            )
-            add_med.save()
-        
-        
-    return render(request, 'Medicine/add_medicine.html')
+                description=description,
 
+            )
+            post.save()
+    medicine = MedicineDrugForm()
+
+    return render(request, 'Medicine/add_medicine.html',context={
+        'form': medicine,
+   
+})
+
+    # บันทึก ลง DB
+    # if request.method == 'POST':
+        
+    #     med_id = request.POST.get('med_id')
+    #     name = request.POST.get('name')
+    #     amount = request.POST.get('amount')
+    #     types = request.POST.get('types')
+    #     description = request.POST.get('description')
+       
+    #     if types == 'Drug':
+    #         add_med = Drug(
+    #             drug_id=med_id,
+    #             name=name,
+    #             amount=amount,
+    #             description=description,
+    #         )
+    #         add_med.save()
+    #     else:
+    #         add_med = Med_supply(
+    #             sup_id=med_id,
+    #             name=name,
+    #             amount=amount,
+    #             description=description,  
+    #         )
+    #         add_med.save()     
+    # return render(request, 'Medicine/add_medicine.html',context={
+    #     'form': medicine,
+   
+    # })
+# --------------------------------
 def update_medicine(request):
     return render(request, 'Medicine/update_medicine.html')
 
@@ -41,8 +72,3 @@ def home_medicine(request):
 
 def comfirm_dispensing(request):
     return render(request, 'Medicine/comfirm_dispensing.html')
-
-
-
-    
-
