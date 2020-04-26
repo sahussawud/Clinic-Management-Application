a2192110ee48a9ec70dd5d588779d19965501a50
sahussawud <sahussawud@hotmail.com>
2020-04-26 23:51:39 +0700

    Update urls.py

--- CTHS/Treatment/urls.py
+++ CTHS/Treatment/urls.py
@@ -6,13 +6,17 @@ urlpatterns = [
     path('patient/find/', views.find_patient, name = 'find_patient'),
     path('patient/create/', views.create_patient, name = 'create_patient'),
     path('patient/update/<int:patient_id>/', views.update_patient, name = 'update_patient'),
+
     path('patient/update/cd/', views.Conginetal_diseaseWithoutPatientAPIView.as_view(), name='cd_api'),
     path('patient/update/<int:patient_id>/cd_api/', views.Conginetal_diseaseAPIView.as_view(), name='cd_api'),
     path('patient/update/drug/', views.DrugWithoutPatientAPIView.as_view(), name='drug_api'),
     path('patient/search/', views.PatientSearchAPIView.as_view(), name='patient_api'),
     path('patient/search/<int:patient_id>/', views.PatientAPIView.as_view(), name='patient_api'),
     path('patient/update/<int:patient_id>/drug_api/', views.DrugAPIView.as_view(), name='drug_api'),
+
     path('create/<int:patient_id>/', views.create_treatment, name = 'create_treatment'),
+    path('create/<int:treatment_id>/diagnosis/', views.diagnosis_treatment, name = 'create_diagnosis'),
+
     path('find/', views.find_treatment, name = 'find_treatment'),
     path('', views.home_treatment, name = 'home_treatment'),
     path('diagnosis/', views.home_diagnosis, name = 'home_diagnosis'),
