23b13e9e8946f723fdff4ad851e14665be903737
bbb <borrabeer@outlook.com>
2020-04-27 17:35:48 +0700

    Update serializers.py

--- CTHS/Treatment/serializers.py
+++ CTHS/Treatment/serializers.py
@@ -46,11 +46,11 @@ class DiagnosisSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class TreatmentSerializer(serializers.ModelSerializer):
-    patient_condition = serializers.CharField(source="get_patient_condition_display")
     diagnosis = DiagnosisSerializer()
+    patient_p_id = PatientSerializer()
     class Meta:
         model = Treatment
-        fields = ['cn', 'patient_condition', 'diagnosis', 'patient_p_id']
+        fields = ['cn', 'diagnosis', 'patient_p_id']
         read_only_fields = ['cn']
 
 class DrugMedSupplySerializer(serializers.Serializer):
