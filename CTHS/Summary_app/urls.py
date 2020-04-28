a7adcaf28debd9c318955d588311a0dd0f8aee05
bbb <borrabeer@outlook.com>
2020-04-28 23:43:58 +0700

    update patient report api

--- CTHS/Summary_app/urls.py
+++ CTHS/Summary_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.dashboard, name = 'dashboard'),
     path('report/api/', views.ReportAPIView.as_view(), name = 'report_api'),
     path('report/type_api/', views.ReportTypeAPIView.as_view(), name = 'report_api'),
+    path('report/patient_api/', views.ReportPatientAPIView.as_view(), name = 'report_api'),
 ] 
